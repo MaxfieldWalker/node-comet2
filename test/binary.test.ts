1d286d7c0b82826851fc6fe3c9a086b34255f72a
Maxfield Walker <maxfield.walker@gmail.com>
2017-03-06 13:06:00 +0900

    add binary test cases not using GR8

--- test/binary.test.ts
+++ test/binary.test.ts
@@ -5,6 +5,8 @@ import { Comet2 } from "../src/comet2/comet2";
 import { Output } from "../src/comet2/io";
 import { Comet2Option } from "../src/comet2/option";
 import { read } from "../src/io/reader";
+import * as _ from "lodash";
+import * as Immutable from "immutable";
 
 function readLines(path: string): Array<string> {
     const buf = read(path);
@@ -17,24 +19,38 @@ const createOutput = (lines: Array<string>) => (s: string) => {
     lines.push(s);
 };
 
-function binaryTest(sourcePath: string, expectedPath: string) {
-    const option: Comet2Option = {
-        useGR8AsSP: true
-    };
+function binaryTest(sourcePath: string, expectedPath: string, option?: Comet2Option) {
+    test("test: " + sourcePath, () => {
+        const actualLines: Array<string> = [];
+        const output: Output = createOutput(actualLines);
 
-    const actualLines: Array<string> = [];
-    const output: Output = createOutput(actualLines);
+        const comet2 = new Comet2(option, undefined, output);
+        comet2.start(sourcePath);
 
-    const comet2 = new Comet2(option, undefined, output);
-    comet2.start(sourcePath);
+        const expectedLines = readLines(expectedPath);
 
-    const expectedLines = readLines(expectedPath);
-
-    assert.deepEqual(actualLines, expectedLines);
+        assert.deepEqual(actualLines, expectedLines);
+    });
 }
 
 suite("binary test", () => {
-    test("test", () => {
-        binaryTest("./test/testdata/normal_01.com", "./test/testdata/normal_01.expected.txt");
+    suite("not use GR8", () => {
+        const fileNames = (n: number) => {
+            // 0埋めする
+            // 1  -> "01"
+            // 12 -> "12"
+            const nn = _.padStart(n.toString(), 2, "0");
+            return [`./test/testdata/normal_${nn}.com`, `./test/testdata/normal_${nn}.expected.txt`]
+        }
+
+        const cases = [1, 2, 4, 5, 6, 7, 11];
+        const option: Comet2Option = {
+            useGR8AsSP: false
+        };
+
+        for (const n of cases) {
+            const [sourcePath, expectedPath] = fileNames(n);
+            binaryTest(sourcePath, expectedPath, option);
+        }
     });
 });
