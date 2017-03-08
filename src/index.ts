096771a43163c8c6eae6dd06afcd98f549dc3da3
Maxfield Walker <maxfield.walker@gmail.com>
2017-03-08 12:03:47 +0900

    add export objects

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@
 
 
 export { Comet2 } from "./comet2/comet2";
+export { Comet2Option } from "./comet2/option";
+export { Register16bit } from "./parts/register16bit";
+export { Flag } from "./parts/flag";
+export { Input, Output } from "./comet2/io";
